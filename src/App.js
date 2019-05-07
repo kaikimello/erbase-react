3d0a3d88244cf015482ab92d48a9b2510916f52e
Kaiki Mello dos Santos <kaiki.mello@gmail.com>
2019-05-07 11:41:05 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { async } from 'q';
+import Header from './components/Header';
+import Info from './components/info';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends React.Component {
+  state = {
+    username: ""
+
+  }
+
+  procuraGitHub = async () => {
+    const {username} = this.state
+    const ghUrl = `http://api.github.com/users/${username}`;
+    const response = await fetch(ghUrl)
+    const {login, html_url, name, public_repos} = await response.json()
+    console.log(login)
+    this.setState({ login, html_url, name, public_repos})
+ 
+
+  }
+
+  onChange = (evento) => {
+    this.setState({ username: evento.target.value })
+  }
+
+  render() {
+    const { username, login, html_url, name, public_repos } = this.state
+    const subtitle= "Encontre repositórios, seguidores e mais apenas pelo nome de usuário";
+  
+    return (
+      <div className="container">
+        <Header title="GitHub Fetch" subtitle={subtitle}/>
+        <input onChange={this.onChange} value={username} className="search-bar" type="text"/> 
+        <button onClick ={this.procuraGitHub}>Procurar</button>
+        {
+
+          login &&(
+            <Info 
+            login={login} 
+            html_url={html_url} 
+            name={name} 
+            public_repos={public_repos}
+          />
+          )
+        }
+        
+      </div>
+    );
+
+  }
+
 }
 
 export default App;
