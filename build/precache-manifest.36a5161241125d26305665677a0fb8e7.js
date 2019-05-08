1a0a3bd7b829ac75aabc53b07fed7dece7000728
Kaiki Mello dos Santos <kaiki.mello@gmail.com>
2019-05-08 09:40:57 -0300

    terceiro commit

---
+++ build/precache-manifest.36a5161241125d26305665677a0fb8e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52b31bbf771403a3f2368c8515ff46e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee1e17b8ef46187c8304",
+    "url": "/static/css/main.5e50a990.chunk.css"
+  },
+  {
+    "revision": "9ecd1974d67bcbe6037f",
+    "url": "/static/js/2.5040d105.chunk.js"
+  },
+  {
+    "revision": "ee1e17b8ef46187c8304",
+    "url": "/static/js/main.9e050ee1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
